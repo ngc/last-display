e4151557ae3e4fb833473981ddb5bbe1f1b192be
Nathan Coulas <contact.nathancoulas@gmail.com>
2020-09-03 17:02:32 -0400

    templates now work

--- main.py
+++ main.py
@@ -6,34 +6,31 @@ from time import sleep
 from lastfmwrapper import lastfm
 import sys
 import urllib.request
-
+from PIL import Image, ImageFilter
+rounds = 0
 """Generates HTML as specified to be rendered by w3m in the terminal"""
 def generate_html(user):
+    global rounds
+    rounds += 1
+
     src = lastfm.get_recent_track(user)
+    image_src = src['image'][3]['#text']
+    urllib.request.urlretrieve(image_src, "__coverimg__.png")
+    cover_img = Image.open("__coverimg__.png")
+    background_img = cover_img.resize((1000, 1000), Image.ANTIALIAS)
+    background_img = background_img.filter(ImageFilter.GaussianBlur(10))
+    background_img.save("__background_img__.png")
+
     track_name = src['name']
     track_artist = src['artist']['#text']
     image_src = src['image'][3]['#text']
 
-    raw_html = """
-    <html>
-    <head>
-    </head>
-    <body>
-    <br>
-    <img src="{0}">
-    <br>
-    Now Playing: {2} | {1}
-    </body>
-    </html>
-    """.format(image_src, track_name, track_artist, os.getcwd() + "/__display__.html")
+    with open('template.html', 'r') as file:
+        raw_html = file.read().format(image_src, track_name, track_artist, (os.getcwd() + "/__display__.html"), rounds)
+
     f = open("__display__.html", "w")
     f.write(raw_html)
     f.close()
-
-def generate_feh(user):
-    src = lastfm.get_recent_track(user)
-    image_src = src['image'][3]['#text']
-    urllib.request.urlretrieve(image_src, "__coverimg__.png")
 
 """Argument Parsing"""
 parser = argparse.ArgumentParser(description="""Display information for a user's last.fm profile in the terminal""")
