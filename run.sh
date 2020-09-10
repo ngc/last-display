d1f723002f211a3c574acb125e1b1edc5c33483c
Nathan Coulas <contact.nathancoulas@gmail.com>
2020-09-10 15:25:33 -0400

    Prevent two windows from opening if both chromium and chromium-browser are installed

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 : ${1?"Usage: USERNAME Needed"}
 nohup python3.8 main.py --user $1 > /dev/null &
-{
+if chromium-browser --help > /dev/null; then
     chromium-browser --app="http://0.0.0.0:8099/__display__.html" > /dev/null &
-} && {
+else
     chromium --app="http://0.0.0.0:8099/__display__.html" > /dev/null &
-}
+fi
+
